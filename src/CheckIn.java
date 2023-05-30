dd63bb770a971e208f8094677d97cba68976ad11
Max <wermelskirchenmaximilian@gmail.com>
2023-05-30 09:23:32 +0200

    v0.22 in progresss

--- src/CheckIn.java
+++ src/CheckIn.java
@@ -1,5 +1,33 @@
-import java.util.Queue;
-import java.util.Timer;
+import java.util.ArrayList;
 
-public class CheckIn {
+public class CheckIn extends Hall {
+
+    private ArrayList<String> check;
+
+
+
+    public CheckIn(int number) {
+        super(number);
+        this.check = new ArrayList<String>();
+    }
+
+    @Override
+    public int getNumber() {
+        return super.getNumber();
+    }
+
+    @Override
+    public ArrayList<String> getList() {
+        return super.getList();
+    }
+
+    @Override
+    public int getListSize() {
+        return super.getListSize();
+    }
+    public int getLast(){
+        int last = check.size()-1;
+        return last;
+    }
 }
+
